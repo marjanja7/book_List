1b305f353f1917bf1f294d7e11847d8e103d8339
marjanja7 <Marjanja7@mail.ru>
2023-06-28 13:55:49 -0400

    Book_List

--- script.js
+++ script.js
@@ -3,37 +3,41 @@ const books = [
       title: 'Design Patterns: Elements of Reusable Object-Oriented Software',
       authors: 'Erich Gamma, John Vlissides, Ralph Johnson, Richard Helm',
       year: '1994',
-      image: './81gtKoapHFL.jpg'
+      image: './book1.jpg'
     },
     {
       title: 'JavaScript: The Good Parts',
       authors: 'Douglas Crockford',
       year: '2008',
-      image: './81kqrwS1nNL.jpg'
+      image: './book2.jpg'
     },
     {
       title:
       'JavaScript Patterns: Build Better Applications with Coding and Design Patterns',
       authors: 'Stoyan Stefanov',
       year: 2008,
-      image: './51+Siphz7AL._SX377_BO1,204,203,200_.jpg'
+      image: './book3.jpg'
     },
     {
       title:
       'JavaScript: The Definitive Guide: Activate Your Web Pages (Definitive Guides)',
       authors: 'David Flanagan',
       year: 2011,
-      image: './51WD-F3GobL._SX379_BO1,204,203,200_.jpg'
+      image: './book4.jpg'
     }
     ]
     const containersBooks = document.getElementById("bookList")
     books.forEach((book)=> {
         containersBooks.innerHTML += `
             <div class="containerBook">
-            <h2>${book.title}</h2>
-            <p class="year">${book.year}</p>
-            <p class="authors">${book.authors}</p>
-            <p>${book.image}</p>
+              <p > <img class="image" src="${book.image}"/></p>
+              <h2>${book.title}</h2>
+              <p class="year">${book.year}</p>
+              <p class="authors">${book.authors}</p>
+              <div class="buttons">
+                <button class="button-chanch">Изменить</button>
+                <button class="button-delete">Удалить</button>
+              </div>
             </div>
             `
             })
