4e324092c815676903f293c4ebd2168619d046a0
marjanja7 <Marjanja7@mail.ru>
2023-08-10 07:34:57 -0400

    добвила обновление книги

--- script.js
+++ script.js
@@ -11,30 +11,32 @@ function closeContainerForm () {
  openFormButton.addEventListener('click', openContainerForm )
  closeFormButton.addEventListener('click', closeContainerForm)
 
-const books = [
+let counter = 0
+let currentBookId
+let books = [
     {
-      id: 1,
+      id: counter++,
       title: 'Design Patterns: Elements of Reusable Object-Oriented Software',
       authors: 'Erich Gamma, John Vlissides, Ralph Johnson, Richard Helm',
       year: '1994',
       image: './book1.jpg'
     },
     {
-      id: 2,
+      id: counter++,
       title: 'JavaScript: The Good Parts',
       authors: 'Douglas Crockford',
       year: '2008',
       image: './book2.jpg'
     },
     {
-      id: 3,
+      id: counter++,
       title: 'JavaScript Patterns: Build Better Applications with Coding and Design Patterns',
       authors: 'Stoyan Stefanov',
       year: 2008,
       image: './book3.jpg'
     },
     {
-      id: 4,
+      id: counter++,
       title: 'JavaScript: The Definitive Guide: Activate Your Web Pages (Definitive Guides)',
       authors: 'David Flanagan',
       year: 2011,
@@ -54,7 +56,7 @@ const books = [
               <p class="year">${book.year}</p>
               <p class="authors">${book.authors}</p>
               <div class="buttons">
-                <button class="button-chanch" ">Изменить</button>
+                <button class="button-change" id="buttonChange-${book.id}">Изменить</button>
                 <button class="button-delete" onclick="deleteBook(${book.id})">Удалить</button>
               </div>
             </div>
@@ -87,6 +89,7 @@ function deleteBook(id) {
     const booksJson = JSON.stringify(books)
     localStorage.setItem('books', booksJson)
 }
+
 const buttonSaveBook = document.getElementById('button-save')
 
 function saveBook() {
@@ -95,21 +98,25 @@ function saveBook() {
     const yearValue = document.getElementById('year').value
     const imageValue = document.getElementById('image').value
 
-    
-    const book = {
-        title: titleValue,
-        authors: authorsValue,
-        year: yearValue,
-        image: imageValue
-    }
-    books.push(book)
-    
-    renderBooks ()
-    clearForm ()
-    closeContainerForm ()
+if (titleValue === '' || authorsValue === '' || yearValue === '' || imageValue === '') {
+  document.getElementById('error-container').style.display = 'flex'
+  return
+}
 
-    const booksJson = JSON.stringify(books)
-    localStorage.setItem('books', booksJson)
+  const book = {
+    id: counter++,
+    title: titleValue,
+    authors: authorsValue,
+    year: yearValue,
+    image: imageValue
+    }
+  books.push(book)
+  renderBooks ()
+  clearForm ()
+  closeContainerForm ()
+
+  const booksJson = JSON.stringify(books)
+  localStorage.setItem('books', booksJson)
 } 
 
 buttonSaveBook.addEventListener('click', saveBook )
@@ -118,6 +125,62 @@ const booksJson = localStorage.getItem('books')
 if (booksJson) {
   books = JSON.parse(booksJson)
 }
-
 renderBooks()
 
+const editContainer = document.getElementById('container-edit')
+const closeEditButton = document.getElementById ('button-cancel')
+
+function editInput (book) {
+    document.getElementById('update-title').value = book.title
+    document.getElementById('update-authors').value = book.authors
+    document.getElementById('update-year').value = book.year
+    document.getElementById('update-image').value = book.image
+}
+
+function openEditContainer (id) {
+  currentBookId = id
+  editContainer.style.display = 'flex'
+  const book = books.find(b => b.id ===id)
+  editInput (book)
+}
+
+function closeEditContainer () {
+  editContainer.style.display = 'none'
+}
+closeEditButton.addEventListener('click', closeEditContainer)
+
+books.forEach((book) => {
+  let openEditButton = document.getElementById(`buttonChange-${book.id}`)
+  openEditButton.addEventListener('click', () => {
+    openEditContainer(book.id)
+  })
+})
+
+
+const updateButton = document.getElementById('button-edit')
+updateButton.addEventListener('click', makeUpdate)
+
+
+function makeUpdate () {
+  const updateTitle = document.getElementById('update-title').value
+  const updateAuthors = document.getElementById('update-authors').value
+  const updateYear = document.getElementById('update-year').value
+  const updateImage = document.getElementById('update-image').value
+
+  let editBook = {
+    id: currentBookId,
+    title: updateTitle,
+    authors: updateAuthors,
+    year: updateYear,
+    image: updateImage
+  }
+  const book = books.find((b) => {
+    return b.id === currentBookId
+  })
+  const bookIndex = books.index0f(book)
+  books.splise(bookIndex, 1, editBook)
+  renderBooks()
+  saveToLocalStorage()
+  closeEditContainer()
+ }
+ 
